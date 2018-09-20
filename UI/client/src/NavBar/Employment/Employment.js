694860e32d8f347427d61ebf764170443e5ae73e
William Cottam <wecottam@yahoo.com>
2018-09-20 11:51:41 -0600

    Finished employment job app form

--- UI/client/src/NavBar/Employment/Employment.js
+++ UI/client/src/NavBar/Employment/Employment.js
@@ -6,296 +6,157 @@ const Employment = () => {
             <h1>Job Application</h1>
             <form>
                 <h3>Personal Information</h3>
-                    <div className="col-lg-6 col-md-12">
-                        <div className="form-group has-error has-danger">
+                    <div>
+                        <div>
                             <input type="text"
-                                   className="form-control"
                                    name="firstname"
                                    placeholder="First Name"
                                    required=""/>
                         </div>
-                        <div className="form-group">
+                        <div>
                             <input type="text"
-                                   className="form-control"
                                    name="lastname"
                                    placeholder="Last Name"
                                    required=""/>
                         </div>
-                    </div>
-                <div className="hidden-lg">
-                    <br/>
-                </div>
-                <div className="form-group has-error has-danger">
+                    </div><br/>
+                <div>
                     <input type="text"
-                           className="form-control"
-                           name="address" placeholder="Address"
+                           name="address" 
+                           placeholder="Address"
                            required=""/>
                 </div>
-                <div className="form-group">
+                <div>
                     <input type="text"
-                           className="form-control"
                            name="city"
                            placeholder="City"
                            required=""/>
                 </div>
-                <div className="form-group">
+                <div>
                     <input type="text"
-                           className="form-control"
                            name="state"
                            placeholder="State"
                            required="" maxLength="2"/>
                 </div>
-                <div className="form-group">
+                <div>
                     <input type="text"
-                           className="form-control"
                            name="zip"
                            placeholder="Zip"
                            required=""
                            maxLength="5"/>
-                </div>
-                <div className="hidden-lg">
-                    <br/>
-                </div>
-                <div className="form-group">
+                </div><br/>
+                <div>
                     <input type="tel"
-                           className="form-control"
                            name="phone"
                            placeholder="Phone"
                            data-format="(999) 999-9999"
                            required=""/>
                 </div>
-                <div className="form-group">
+                <div>
                     <input type="tel"
-                           className="form-control"
                            name="alternate"
                            placeholder="Alternate Phone"
                            data-format="(999) 999-9999"/>
                 </div>
-                <div className="form-group">
+                <div>
                     <input type="email"
-                           className="form-control"
                            name="email"
                            placeholder="Email Address"
                            required=""/>
                 </div><br/>
-                <div className="form-group has-error has-danger">
-                    <label className="control-label">
+                <div>
+                    <label>
                         Date of Birth
                     </label><br/>
                     <input type="date"
-                           className="form-control"
                            name="dob"
                            max="2005-12-31"
                            required=""/>
-                </div>
-                <div className="well">
+                </div><br/>
+                <div>
                     <h3>Availability Information</h3>
-                        <div className="row">
-                            <div className="col-lg-6 col-md-12">
-                             <div className="form-group">
+                        <div>
+                            <div>
+                                <div>
+                                    <input type="radio"
+                                            name="available"/>
+                                Available Full Time (28 or more hrs per week)
+                                </div>
+                                <div>
                                  <input type="radio"
                                         name="available"/>
-                                    {/* <button type="button" className="btn btn-default">
-                                        <span className="text-muted">N</span>
-                                    </button> */}
-                               Available Full Time (28 or more hrs per week)
-                             </div>
-                            <div className="form-group">
-                                <input type="radio"
-                                       name="available"/>
-                                {/* <button type="button" className="btn btn-default">
-                                    <span className="text-muted">N</span>
-                                </button> */}
                                 Available Part Time (less than 28 hrs per week)
+                                </div><br/>
                             </div>
-                            {/* <div className="form-group">
-                                <input type="hidden"
-                                       name="available-overttime"
-                                       value="false"/>
-                                <button type="button" className="btn btn-default">
-                                    <span className="text-muted">N</span>
-                                </button>
-                               Available for Overtime (40+ hours per week)
-                            </div> */}
                         </div>
-                    </div>
-                    {/*<div style="width: 400px">*/}
+                            <div>
+                                <h5>When can you be available to start working?</h5>
+                                    <div>
+                                        <input type="date"
+                                                name="availablestart"
+                                                required=""/>
+                                    </div><br/>
+                            </div>
+                                <h4>What Days Are You Available To Work?</h4>
+                                    <div>
+                                    <input type="checkbox"
+                                            name="Monday"/>
+                                            Monday
+                                    </div>
+                                    <div>
+                                    <input type="checkbox"
+                                            name="Tuesday"/>
+                                            Tuesday
+                                    </div>
+                                    <div>
+                                    <input type="checkbox"
+                                            name="Wednesday"/>
+                                            Wednesday
+                                    </div>
+                                    <div>
+                                    <input type="checkbox"
+                                            name="Thursday"/>
+                                            Thursday
+                                    </div>
+                                    <div>
+                                    <input type="checkbox"
+                                            name="Friday"/>
+                                            Friday
+                                    </div>
+                                    <div>
+                                    <input type="checkbox"
+                                            name="Saturday"/>
+                                            Saturday
+                                    </div><br/>
+                                <h4>What Shift(s) Are You Available To Work?</h4>
+                                <div>
+                                    <input type="checkbox"
+                                            name="Morning"/>
+                                            Morning
+                                    </div><div>
+                                    <input type="checkbox"
+                                            name="Afternoon"/>
+                                            Afternoon
+                                    </div><div>
+                                    <input type="checkbox"
+                                            name="Evening"/>
+                                            Evening
+                                    </div><br/>
                         <div>
-                        <h5>When can you be available to start working?</h5>
-                        <div className="form-group">
-                            <input type="date"
-                                   className="form-control"
-                                   name="availablestart"
-                                   required=""/>
-                        </div>
-                    </div>
-                    <div className="text-primary">
-                        <h4>What Shifts Are You Available To Work?</h4>
-                    </div>
-                    <div>
-                        {/*<table style="width: 100%; text-align: center;">*/}
-                        <table>
-                            <tbody>
-                                <tr>
-                                    {/*<th style="width: 100px;"></th>*/}
-                                    {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Mon</th>*/}
-                                    {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Tue</th>*/}
-                                    {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Wed</th>*/}
-                                    {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Thu</th>*/}
-                                    {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Fri</th>*/}
-                                    {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Sat</th>*/}
-                                    {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Sun</th>*/}
-                                    <th>Mon</th>
-                                    <th>Tue</th>
-                                    <th>Wed</th>
-                                    <th>Thu</th>
-                                    <th>Fri</th>
-                                    <th>Sat</th>
-                                </tr>
-                                <tr>
-                                 {/*<td style="font-weight: 700;">Morning</td>*/}
-                                    <td>Morning</td>
-                                    <td>
-                                        <div data-id="morning|1">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="morning|2">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="morning|3">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="morning|4">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="morning|5">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="morning|6">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="morning|7">
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Day</td>
-                                    <td>
-                                        <div data-id="day|1">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="day|2">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="day|3">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="day|4">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="day|5">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="day|6">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="day|7">
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Evening</td>
-                                    <td>
-                                        <div data-id="evening|1">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="evening|2">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="evening|3">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="evening|4">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="evening|5">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="evening|6">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="evening|7">
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Overnight</td>
-                                    <td>
-                                        <div data-id="overnight|1">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="overnight|2">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="overnight|3">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="overnight|4">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="overnight|5">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="overnight|6">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div data-id="overnight|7">
-                                        </div>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <div>
-                            <span>* Click on the time blocks to change your availability</span>
-                            <div>Available</div>
-                            <div>Unavailable</div>
-                        </div>
-                    </div>
-                    <input type="hidden" name="availability" value="{}"/>
-                        <div className="row">
-                            <div className="col-lg-6 col-md-12">
-                                <div className="form-group"><label className="control-label">Please tell us any other
-                                    scheduling or availability considerations, ie: school, children,
-                                    etc.</label><textarea className="form-control" name="schedulenotes"
-                                                          placeholder="Other Scheduling/Availability Information">
-                                </textarea>
+                            <div>
+                                <div>
+                                    <label>
+                                        Please tell us any other scheduling or availability considerations, ie: school, children, etc.
+                                    </label><br/><br/>
+                                    <textarea rows="10" cols="50" name="schedulenotes"
+                                            placeholder="Other Scheduling/Availability Information">
+                                    </textarea>
                                 </div>
                             </div>
                         </div>
+                </div>
+                <div>
+                    <input type="Submit"/>
                 </div>
             </form>
         </div>

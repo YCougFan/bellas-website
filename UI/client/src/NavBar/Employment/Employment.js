8e9e6d272ea7dd7d5bb03fd2c298959f1d983dd9
William Cottam <wecottam@yahoo.com>
2018-09-19 23:55:04 -0600

    updated Employment application form

--- UI/client/src/NavBar/Employment/Employment.js
+++ UI/client/src/NavBar/Employment/Employment.js
@@ -85,6 +85,7 @@ const Employment = () => {
                     <input type="date"
                            className="form-control"
                            name="dob"
+                           max="2005-12-31"
                            required=""/>
                 </div>
                 <div className="well">
@@ -92,24 +93,22 @@ const Employment = () => {
                         <div className="row">
                             <div className="col-lg-6 col-md-12">
                              <div className="form-group">
-                                 <input type="hidden"
-                                        name="available-fulltime"
-                                        value="false"/>
-                                    <button type="button" className="btn btn-default">
+                                 <input type="radio"
+                                        name="available"/>
+                                    {/* <button type="button" className="btn btn-default">
                                         <span className="text-muted">N</span>
-                                    </button>
+                                    </button> */}
                                Available Full Time (28 or more hrs per week)
                              </div>
                             <div className="form-group">
-                                <input type="hidden"
-                                       name="available-parttime"
-                                       value="false"/>
-                                <button type="button" className="btn btn-default">
+                                <input type="radio"
+                                       name="available"/>
+                                {/* <button type="button" className="btn btn-default">
                                     <span className="text-muted">N</span>
-                                </button>
+                                </button> */}
                                 Available Part Time (less than 28 hrs per week)
                             </div>
-                            <div className="form-group">
+                            {/* <div className="form-group">
                                 <input type="hidden"
                                        name="available-overttime"
                                        value="false"/>
@@ -117,17 +116,16 @@ const Employment = () => {
                                     <span className="text-muted">N</span>
                                 </button>
                                Available for Overtime (40+ hours per week)
-                            </div>
+                            </div> */}
                         </div>
                     </div>
                     {/*<div style="width: 400px">*/}
                         <div>
-                        <h5>How many days after hire date can you start working?</h5>
+                        <h5>When can you be available to start working?</h5>
                         <div className="form-group">
-                            <input type="number"
+                            <input type="date"
                                    className="form-control"
                                    name="availablestart"
-
                                    required=""/>
                         </div>
                     </div>
@@ -138,147 +136,147 @@ const Employment = () => {
                         {/*<table style="width: 100%; text-align: center;">*/}
                         <table>
                             <tbody>
-                            <tr>
-                                {/*<th style="width: 100px;"></th>*/}
-                                {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Mon</th>*/}
-                                {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Tue</th>*/}
-                                {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Wed</th>*/}
-                                {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Thu</th>*/}
-                                {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Fri</th>*/}
-                                {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Sat</th>*/}
-                                {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Sun</th>*/}
-                                <th>Mon</th>
-                                <th>Tue</th>
-                                <th>Wed</th>
-                                <th>Thu</th>
-                                <th>Fri</th>
-                                <th>Sat</th>
-                            </tr>
-                            <tr>
-                                {/*<td style="font-weight: 700;">Morning</td>*/}
-                                <td>Morning</td>
-                                <td>
-                                    <div data-id="morning|1">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="morning|2">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="morning|3">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="morning|4">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="morning|5">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="morning|6">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="morning|7">
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Day</td>
-                                <td>
-                                    <div data-id="day|1">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="day|2">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="day|3">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="day|4">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="day|5">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="day|6">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="day|7">
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Evening</td>
-                                <td>
-                                    <div data-id="evening|1">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="evening|2">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="evening|3">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="evening|4">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="evening|5">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="evening|6">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="evening|7">
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Overnight</td>
-                                <td>
-                                    <div data-id="overnight|1">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="overnight|2">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="overnight|3">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="overnight|4">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="overnight|5">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="overnight|6">
-                                    </div>
-                                </td>
-                                <td>
-                                    <div data-id="overnight|7">
-                                    </div>
-                                </td>
-                            </tr>
+                                <tr>
+                                    {/*<th style="width: 100px;"></th>*/}
+                                    {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Mon</th>*/}
+                                    {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Tue</th>*/}
+                                    {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Wed</th>*/}
+                                    {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Thu</th>*/}
+                                    {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Fri</th>*/}
+                                    {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Sat</th>*/}
+                                    {/*<th style="text-align: center; min-width: 50px; max-width: 150px;">Sun</th>*/}
+                                    <th>Mon</th>
+                                    <th>Tue</th>
+                                    <th>Wed</th>
+                                    <th>Thu</th>
+                                    <th>Fri</th>
+                                    <th>Sat</th>
+                                </tr>
+                                <tr>
+                                 {/*<td style="font-weight: 700;">Morning</td>*/}
+                                    <td>Morning</td>
+                                    <td>
+                                        <div data-id="morning|1">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="morning|2">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="morning|3">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="morning|4">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="morning|5">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="morning|6">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="morning|7">
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Day</td>
+                                    <td>
+                                        <div data-id="day|1">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="day|2">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="day|3">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="day|4">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="day|5">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="day|6">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="day|7">
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Evening</td>
+                                    <td>
+                                        <div data-id="evening|1">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="evening|2">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="evening|3">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="evening|4">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="evening|5">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="evening|6">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="evening|7">
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Overnight</td>
+                                    <td>
+                                        <div data-id="overnight|1">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="overnight|2">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="overnight|3">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="overnight|4">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="overnight|5">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="overnight|6">
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div data-id="overnight|7">
+                                        </div>
+                                    </td>
+                                </tr>
                             </tbody>
                         </table>
                         <div>
